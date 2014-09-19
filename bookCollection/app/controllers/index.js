58d55e786fd88bcda80dc21bfd3ac0d445449163
hiroo takaku <slgerkamp@gmail.com>
2014-09-19 15:21:10 +0900

    bookCollection：Create a book detail view

--- bookCollection/app/controllers/index.js
+++ bookCollection/app/controllers/index.js
@@ -13,10 +13,26 @@ myBooks.add(book);
 // persist it to the database
 book.save();
 
+function showBook(event){
+	var selectedBook = event.source;
+	var args = {
+		title:selectedBook.title,
+		author:selectedBook.author
+	};
+	var bookview = Alloy.createController('bookdetails', args).getView();
+
+    if (OS_IOS) {
+        $.navGroupWin.openWindow(bookview);
+    }
+    if (OS_ANDROID) {
+        bookview.open();
+    }
+}
+
 // Open main window
-if(OS_IOS) { 
-   $.navGroupWin.open(); 
-} 
-if (OS_ANDROID) { 
-   $.index.open(); 
+if(OS_IOS) {
+   $.navGroupWin.open();
+}
+if (OS_ANDROID) {
+   $.index.open();
 }
