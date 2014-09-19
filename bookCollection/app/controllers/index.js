91199341a09f7710f9bcb932731f8bd49cae92ea
hiroo takaku <slgerkamp@gmail.com>
2014-09-19 13:12:23 +0900

    bookCollection：initialize the book collection

--- bookCollection/app/controllers/index.js
+++ bookCollection/app/controllers/index.js
@@ -1,5 +1,22 @@
-function doClick(e) {
-    alert($.label.text);
-}
+// get a reference to our collection
+var myBooks = Alloy.Collections.books;
 
-$.index.open();
+// create a new model
+var book = Alloy.createModel('books',{
+	title : "Great Expectations",
+	author : "Charles Dickens"
+});
+
+// add the book to collection
+myBooks.add(book);
+
+// persist it to the database
+book.save();
+
+// Open main window
+if(OS_IOS) { 
+   $.navGroupWin.open(); 
+} 
+if (OS_ANDROID) { 
+   $.index.open(); 
+}
